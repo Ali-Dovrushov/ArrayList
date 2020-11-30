8b82e1898f5e2c2feab77a650f92194821f430bd
Ali-Dovrushov <69958880+Ali-Dovrushov@users.noreply.github.com>
2020-11-30 10:22:26 +0400

    Update ArrayList.js

--- src/ArrayList.js
+++ src/ArrayList.js
@@ -16,21 +16,26 @@ ArrayList.prototype.myPop = function() {
     const myPopDel = this.array[this.size - 1];
     this.array.length = this.array.length - 1;
     this.size--;
+    
     return myPopDel;
 }
 
 ArrayList.prototype.myPush = function(pushNumber) {
     this.array[this.size] = pushNumber;
+    
     return ++this.size;
 }
 
 ArrayList.prototype.myShift = function() {
     let shift = this.array[0];
+    
     for(let i = 1; i < this.array.length; i++) {
         this.array[i - 1] = this.array[i]
     }
+    
     this.array.length -= 1;
     this.size--;
+    
     return shift;
 }
 
@@ -38,12 +43,15 @@ ArrayList.prototype.myUnShift = function(shiftNumber) {
     for(let i = this.size; i >= 0; i--) {
         this.array[i] = this.array[i - 1];
     }
+    
     this.array[0] = shiftNumber;
+    
     return this.size++;
 }
 
 ArrayList.prototype.myToString = function() {
     let getString = '[';
+    
     for(let i = 0; i < this.size; i++) {
         if (i === (this.size - 1)) {
             getString += this.array[i];
@@ -53,6 +61,7 @@ ArrayList.prototype.myToString = function() {
         }
     }
     getString += ']'
+    
     return getString;
 }
 
@@ -63,19 +72,24 @@ ArrayList.prototype.myClear = function() {
 
 ArrayList.prototype.myReverse = function() {
     const reversedArray = [];
+    
     for(let i = this.array.length - 1, j = 0; i >= 0; i--, j++) {
         reversedArray[j] = this.array[i];
     }
+    
     return reversedArray;
 }
 
 ArrayList.prototype.mySlice = function(from, to) {
     if(from > 0 && to > 0) {
         let slicedArray = [];
+        
         for(let i = from; i < to; i++) {
             slicedArray[slicedArray.length] = this.array[i];
         }
+        
         this.myInit(slicedArray);
+        
         return slicedArray;
     }
 }
